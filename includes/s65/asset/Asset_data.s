9435a520d65fa1eb030041fe3f1af3611d8a64e9
smnjameson <smnjameson@googlemail.com>
2022-07-29 00:18:02 +0100

    Tilemap structs defined and Asset_ImportTilemap written

--- includes/s65/asset/Asset_data.s
+++ includes/s65/asset/Asset_data.s
@@ -29,9 +29,27 @@
 */
 .struct Asset_Charset { id, name, address, colorAddress, palette, colors, indices }
 
+
+/**
+* .data Tilemap
+* The object returned by GetTilemap
+* @namespace Asset
+* 
+* @struct {byte} id The numerical id of the tilemap assigned by Asset_ImportTilemap
+* @struct {string} name The name of the tilemap
+* @struct {word} tilemapAddress The start address for the tile definitions data
+* @struct {word} tiledefAddress The start address for the tile definitions data
+* @struct {List} tilemap A List of bytes containing the tilemap data
+* @struct {List} tiles A List of bytes containing the tile definition data
+*/
+.struct Asset_Tilemap { id, name, tilemapAddress, tiledefAddress, tilemap, tiles }
+
+
 .var Asset_CharList = List()
 .var Asset_SpriteList = List()
+.var Asset_TilemapList = List()
 .var Asset_PreloaderList = List()
+
 
 /**
 * .var Asset_SpriteListMetaTable
@@ -63,4 +81,20 @@
 	.if(num==13) .segment Preload13 [outBin="./sdcard/P13"]
 	.if(num==14) .segment Preload14 [outBin="./sdcard/P14"]
 	.if(num==15) .segment Preload15 [outBin="./sdcard/P15"]
+	.if(num==16) .segment Preload16 [outBin="./sdcard/P16"]
+	.if(num==17) .segment Preload17 [outBin="./sdcard/P17"]
+	.if(num==18) .segment Preload18 [outBin="./sdcard/P18"]
+	.if(num==19) .segment Preload19 [outBin="./sdcard/P19"]
+	.if(num==20) .segment Preload20 [outBin="./sdcard/P20"]
+	.if(num==21) .segment Preload21 [outBin="./sdcard/P21"]
+	.if(num==22) .segment Preload22 [outBin="./sdcard/P22"]
+	.if(num==23) .segment Preload23 [outBin="./sdcard/P23"]
+	.if(num==24) .segment Preload24 [outBin="./sdcard/P24"]
+	.if(num==25) .segment Preload25 [outBin="./sdcard/P25"]
+	.if(num==26) .segment Preload26 [outBin="./sdcard/P26"]
+	.if(num==27) .segment Preload27 [outBin="./sdcard/P27"]
+	.if(num==28) .segment Preload28 [outBin="./sdcard/P28"]
+	.if(num==29) .segment Preload29 [outBin="./sdcard/P29"]
+	.if(num==30) .segment Preload30 [outBin="./sdcard/P30"]
+	.if(num==31) .segment Preload31 [outBin="./sdcard/P31"]
 }
